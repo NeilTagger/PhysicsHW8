44c60ab4165ba8369ea8cf2d3450d315184dff8c
neiliam <neil.tagger@gmail.com>
2021-01-07 02:26:58 +0200

    The project

---
+++ Assets/HangingBoxScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangingBoxScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private float thrust = 5.0f;
+    public GameObject parachute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(transform.right * thrust);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            parachute.SetActive(false);
+        }
+    }
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RocketScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject myself;
+    public Rigidbody2D rb;
+    private float thrust = 400;
+    
+    void Start()
+    {
+        rb.AddForce(transform.right * thrust);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            target.SetActive(false);
+            myself.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
